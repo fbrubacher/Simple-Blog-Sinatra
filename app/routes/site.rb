ec0a98c023e846a620ac12eba9d5a14e772c64f0
Federico Brubacher <fbrubacher@gmail.com>
2010-01-02 23:15:32 -0200

    comment works

--- app/routes/site.rb
+++ app/routes/site.rb
@@ -12,9 +12,21 @@ class Main
 
   get "/:permalink" do 
     permalink = params[:permalink]
+    @comment = Comment.new
     @post = Post.first(:permalink => permalink)
     haml :show
   end
+
+  post "/:permalink/comments" do |permalink| 
+    @post = Post.first(:permalink => permalink) 
+    @comment = @post.comments.build(params[:comment])
+    if @comment.save
+      redirect post_path(@post)
+    else
+      haml :show 
+    end
+  end
+  
   
   post "/" do
     @post = Post.new(params[:post])
