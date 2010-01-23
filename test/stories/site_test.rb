d4b51909d125699b2269dd731811e5b3c2ae2036
Federico Brubacher <fbrubacher@gmail.com>
2010-01-23 12:27:19 -0200

    integration test

--- test/stories/site_test.rb
+++ test/stories/site_test.rb
@@ -5,6 +5,14 @@ class SiteTest < Test::Unit::TestCase
     visit "/"
     assert_contain "Login"
   end
+  
+  def test_login
+    visit "/login"
+    fill_in "email", :with => "fbru02@yahoo.com.ar"  
+    fill_in "password", :with => "jakobs"  
+    click_button "Login"  
+    assert_contain "Logout"
+  end
 
 #  story "As a developer I want to see the homepage so I know Monk is correctly installed" do
 #    scenario "A visitor goes to the homepage" do
